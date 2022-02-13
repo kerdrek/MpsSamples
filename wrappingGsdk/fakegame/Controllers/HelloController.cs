217b61f87e6cd7f0d51f35025848b737c856cc4b
Cedrick Montout <cmontout@microsoft.com>
2022-02-12 16:39:16 -0800

    Add 3 new routes crash/morecpu/morememory

--- wrappingGsdk/fakegame/Controllers/HelloController.cs
+++ wrappingGsdk/fakegame/Controllers/HelloController.cs
@@ -32,5 +32,30 @@ namespace fakegame.Controllers
             Console.WriteLine($"GET /hello/terminate at {DateTime.UtcNow}");
             applicationLifetime.StopApplication();
         }
+
+        [HttpGet("crash")]
+        public void Crash()
+        {
+            Console.WriteLine($"GET /hello/crash at {DateTime.UtcNow}");
+            System.Environment.FailFast("An Expected Error Occurred");
+        }
+
+        [HttpGet("morecpu")]
+        public string MoreCpu()
+        {
+            Console.WriteLine($"GET /hello/morecpu at {DateTime.UtcNow}");
+            Task.Run(() => { while(true){}});
+            return $"Cpu Usage Increased on {Dns.GetHostName()}";
+        }
+
+        [HttpGet("morememory")]
+        public string MoreMemory()
+        {
+            Console.WriteLine($"GET /hello/morememory at {DateTime.UtcNow}");
+            byte[] data = new byte[1024*1024*1024];
+            Array.Clear(data, 0, data.Length);
+            return $"Memory Usage Increased on {Dns.GetHostName()}";
+
+        }
     }
 }
